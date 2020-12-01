b3e576e3a3e8ae6e36dd2d71874147dc268f41cb
Wei Wenyu <74281098+11weiwenyu@users.noreply.github.com>
2020-12-02 03:36:57 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,54 @@
-	$(function(){
-									//获取点击事件的对象
-									$("nav li a").click(function(){
-										//获取要显示或隐藏的对象
-										var divShow = $(".b1").children('.f1');
-										//判断当前对象是否被选中，如果没选中的话进入if循环
-										if (!$(this).hasClass('a1')) {
-											//获取当前对象的索引
-											var index = $(this).index();
-											//当前对象添加选中样式并且其同胞移除选中样式；
-											$(this).addClass('a1').siblings('a').removeClass('a1');
-											//索引对应的div块显示
-											$(divShow[index]).show();
-											//索引对应的div块的同胞隐藏
-											$(divShow[index]).siblings('.f1').hide();
-										}
-									});
-								});
-								
+	jQuery(document).ready(function($){
+		var slidesWrapper = $('.cd-hero-slider');
+	
+
+		//check if a .cd-hero-slider exists in the DOM 
+		if ( slidesWrapper.length > 0 ) {
+				var sliderNav = $('.cd-slider-nav'),
+				navigationMarker = $('.cd-marker'),
+				slidesNumber = slidesWrapper.children('section').length,
+				visibleSlidePosition = 0;
+			//change visible slide
+			sliderNav.on('click', 'section', function(event){
+				event.preventDefault();
+				var selectedItem = $(this);
+				if(!selectedItem.hasClass('selected')) {
+					// if it's not already selected
+					var selectedPosition = selectedItem.index(),
+						activePosition = slidesWrapper.find('section.selected').index();
+					
+					if( activePosition < selectedPosition) {
+						nextSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, selectedPosition);
+					} else {
+						prevSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, selectedPosition);
+					}
+
+		function nextSlide(visibleSlide, container, pagination, n){
+			visibleSlide.removeClass('selected from-left from-right').addClass('is-moving').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				visibleSlide.removeClass('is-moving');
+			});
+	
+
+			container.children('section').eq(n).addClass('selected from-right').prevAll().addClass('move-left');
+			checkVideo(visibleSlide, container, n);
+		}
+	
+
+		function prevSlide(visibleSlide, container, pagination, n){
+			visibleSlide.removeClass('selected from-left from-right').addClass('is-moving').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				visibleSlide.removeClass('is-moving');
+			});
+	
+
+			container.children('li').eq(n).addClass('selected from-left').removeClass('move-left').nextAll().removeClass('move-left');
+			checkVideo(visibleSlide, container, n);
+		}
+	
+
+		function updateSliderNavigation(pagination, n) {
+			var navigationDot = pagination.find('.selected');
+			navigationDot.removeClass('selected');
+			pagination.find('li').eq(n).addClass('selected');
+		}
+	
+
